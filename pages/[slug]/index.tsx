5ad93f1bf2d0486115a8fdeba9d33f3dfd964005
hugodnpm <hugosc@gmail.com>
2022-07-02 17:49:05 -0300

    add urql in package.json

--- pages/[slug]/index.tsx
+++ pages/[slug]/index.tsx
@@ -1,35 +1,35 @@
-import request from 'graphql-request'
-import type { GetStaticProps, NextPage } from 'next'
-import Head from 'next/head'
-import Link from 'next/link'
-import { useAccount } from '../../contexts/AccountContext'
+import request from "graphql-request";
+import type { GetStaticProps, NextPage } from "next";
+import Head from "next/head";
+import Link from "next/link";
+import { useAccount } from "../../contexts/AccountContext";
 
 const Home: NextPage = () => {
-  const account = useAccount()
+  const account = useAccount();
   return (
     <div>
       <Head>
         <title>{account?.friendlyName}</title>
       </Head>
-      <h1>Welcome {account?.friendlyName}</h1>
+      <h1>Welcome hugo {account?.friendlyName}</h1>
       <p>
-        <Link href='/about'>
-          <a>About</a>
+        <Link href="/about">
+          <a>hugo-About</a>
         </Link>
       </p>
     </div>
-  )
-}
+  );
+};
 
 export async function getStaticPaths() {
-  return { paths: [], fallback: 'blocking' }
+  return { paths: [], fallback: "blocking" };
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
-  const slug = context?.params?.slug
+  const slug = context?.params?.slug;
   try {
     const { account } = await request(
-      'https://api.learn49.com/graphql',
+      "https://api.learn49.com/graphql",
       `
   query{
     account: getAccountSettingsByDomain(domain: "${slug}.learn49.com"){
@@ -37,20 +37,20 @@ export const getStaticProps: GetStaticProps = async (context) => {
       friendlyName
     }
   }`
-    )
+    );
     return {
       props: {
-        account
+        account,
       },
-      revalidate: 60
-    }
+      revalidate: 60,
+    };
   } catch (err) {}
   return {
     props: {
-      account: null
+      account: null,
     },
-    revalidate: 60
-  }
-}
+    revalidate: 60,
+  };
+};
 
-export default Home
+export default Home;
