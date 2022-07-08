b95739aa97fef2e7c3e26621052a950593d2dadb
hugodnpm <hugosc@gmail.com>
2022-07-08 09:36:41 -0300

    add .env and .env-test

--- pages/[slug]/index.tsx
+++ pages/[slug]/index.tsx
@@ -2,16 +2,32 @@ import request from "graphql-request";
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
+import { useQuery } from "urql";
 import { useAccount } from "../../contexts/AccountContext";
 
+const teste = `query{
+  getAccountSettingsByDomain(domain: "devpleno.learn49.com"){
+    id
+    friendlyName
+  }
+}`;
 const Home: NextPage = () => {
+  const [result, reexecuteQuery] = useQuery({
+    query: teste,
+  });
+
+  const { data, fetching, error } = result;
+
+  if (fetching) return <p>Loading...</p>;
+  if (error) return <p>Oh no... {error.message}</p>;
   const account = useAccount();
   return (
     <div>
       <Head>
         <title>{account?.friendlyName}</title>
       </Head>
-      <h1>Welcome hugo {account?.friendlyName}</h1>
+      <h1>Welcome hugo! {account?.friendlyName}</h1>
+      <pre>{JSON.stringify(data)}</pre>
       <p>
         <Link href="/about">
           <a>hugo-About</a>
