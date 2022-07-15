9e4e8f170cfc0472a1a5f60341d3b1064b895f86
Renato Siqueira <renatoelysiqueira@gmail.com>
2022-07-15 01:24:47 -0300

    Fix: to ignore url rewrite on public folders

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -2,6 +2,11 @@ import { NextFetchEvent, NextRequest, NextResponse } from 'next/server'
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
   const url = req.nextUrl.clone()
+  const path = req.nextUrl.pathname
+
+  const isExcluded = path.startsWith('/api') || path.startsWith('/static') || path.includes('.')
+  if (isExcluded) return
+
   const hostname = req.headers.get('host')
 
   const slug = hostname?.split('.')[0]
