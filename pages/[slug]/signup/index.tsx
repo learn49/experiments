7b0fb2a23b7efe28e0433676b577f25d2a45a80f
Kleber Freire <ch4r4d4@gmail.com>
2022-07-19 13:47:48 -0300

    criando o recaptcha

--- pages/[slug]/signup/index.tsx
+++ pages/[slug]/signup/index.tsx
@@ -1,4 +1,6 @@
+import { createRef } from "react";
 import { Formik, Field, Form, FormikHelpers } from "formik";
+import * as Yup from "yup";
 import ReCaptcha from "react-google-recaptcha";
 import Link from "next/link";
 import Image from "next/image";
@@ -6,7 +8,6 @@ import { Button } from "./Button";
 import { Label } from "./Label";
 
 import DevplenoLogo from "../../../public/devpleno.svg";
-import { useRef } from "react";
 
 interface SignupValues {
   firstName: string;
@@ -18,17 +19,34 @@ interface SignupValues {
 
 export default function Signup() {
   const siteKey = process.env.RECAPTCHA_SITE_KEY;
-  const captchaRef = useRef<ReCAPTCHA | undefined>();
+  const captchaRef = createRef<ReCaptcha>();
 
   const handleSubmit = (
     values: SignupValues,
     { setSubmitting }: FormikHelpers<SignupValues>
   ) => {
+    const results = {
+      ...values,
+      captcha: captchaRef.current?.getValue(),
+    };
     setTimeout(() => {
-      alert(JSON.stringify(values, null, 2));
+      alert(JSON.stringify(results, null, 2));
       setSubmitting(false);
     }, 500);
   };
+
+  const SignupSchema = Yup.object().shape({
+    firstName: Yup.string().required("Campo obrigatório"),
+    lastName: Yup.string().required("Campo obrigatório"),
+    email: Yup.string().required("Campo obrigatório").email("Email inválido"),
+    password: Yup.string()
+      .required("Campo obrigatório")
+      .min(6, "Mínimo de 6 caracteres"),
+    acceptTerms: Yup.bool().oneOf(
+      [true],
+      "Deve-se aceitar os Termos & Condições"
+    ),
+  });
 
   return (
     <div className={`min-h-screen bg-gray-50 dark:bg-gray-900 `}>
@@ -50,80 +68,106 @@ export default function Signup() {
                 password: "",
                 acceptTerms: false,
               }}
+              validationSchema={SignupSchema}
               onSubmit={handleSubmit}
             >
-              <Form>
-                <Label>
-                  <span className="block">Nome</span>
-                  <Field
-                    className="mt-1 border border-opacity-50 w-full text-sm rounded-md p-2 "
-                    type="text"
-                    id="firstName"
-                    placeholder="Nome"
-                    name="firstName"
-                  />
-                </Label>
-                <Label className="mt-2 block">
-                  <span className="block">Último Nome</span>
-                  <Field
-                    className="mt-1 border border-opacity-50 w-full text-sm rounded-md p-2"
-                    type="text"
-                    id="lastName"
-                    placeholder="Nome"
-                    name="lastName"
-                  />
-                </Label>
-                <Label className="mt-2">
-                  <span className="block">Email</span>
-                  <Field
-                    className="mt-1 border border-opacity-50 w-full text-sm rounded-md p-2"
-                    type="email"
-                    id="email"
-                    placeholder="Digite seu email"
-                    name="email"
-                  />
-                </Label>
-                <Label className="mt-2">
-                  <span className="block">Senha</span>
-                  <Field
-                    className="mt-1 border border-opacity-50 w-full text-sm rounded-md p-2"
-                    type="password"
-                    id="password"
-                    placeholder="Digite sua senha"
-                    name="password"
-                  />
-                </Label>
-                <Label check className="mt-2">
-                  <Field className="" type="checkbox" name="acceptTerms" />
-                  <span className="ml-2">
-                    Criar uma conta significa que você concorda com nossos{" "}
-                    <Link href="/accept-terms" passHref>
-                      <a className="text-gray-800 font-medium hover:underline hover:text-gray-600">
-                        Termos de Serviço,
-                      </a>
-                    </Link>
-                    <Link href="/policies" passHref>
-                      <a className="text-gray-800 font-medium hover:underline hover:text-gray-600">
-                        Política de Privacidade
-                      </a>
-                    </Link>{" "}
-                    e nossas{" "}
-                    <Link href="/policies" passHref>
-                      <a className="text-gray-800 font-medium hover:underline hover:text-gray-600">
-                        Configurações de Notificações.
-                      </a>
-                    </Link>
-                  </span>
-                </Label>
-                <Button
-                  className="mt-4 w-full bg-purple-600 rounded-md text-white text-lg font-bold py-3 hover:bg-purple-600"
-                  block={false}
-                  size="large"
-                  type="submit"
-                >
-                  Cadastrar
-                </Button>
-              </Form>
+              {({ errors }) => (
+                <Form>
+                  <Label>
+                    <span className="block">Nome</span>
+                    <Field
+                      className="mt-1 border border-opacity-50 w-full text-sm rounded-md p-2 "
+                      type="text"
+                      id="firstName"
+                      placeholder="Nome"
+                      name="firstName"
+                    />
+                    {errors.firstName ? (
+                      <div className="text-xs text-red-300">
+                        {errors.firstName}
+                      </div>
+                    ) : null}
+                  </Label>
+                  <Label className="mt-2 block">
+                    <span className="block">Último Nome</span>
+                    <Field
+                      className="mt-1 border border-opacity-50 w-full text-sm rounded-md p-2"
+                      type="text"
+                      id="lastName"
+                      placeholder="Nome"
+                      name="lastName"
+                    />
+                    {errors.lastName ? (
+                      <div className="text-xs text-red-300">
+                        {errors.lastName}
+                      </div>
+                    ) : null}
+                  </Label>
+                  <Label className="mt-2">
+                    <span className="block">Email</span>
+                    <Field
+                      className="mt-1 border border-opacity-50 w-full text-sm rounded-md p-2"
+                      type="email"
+                      id="email"
+                      placeholder="Digite seu email"
+                      name="email"
+                    />
+                    {errors.email ? (
+                      <div className="text-xs text-red-300">{errors.email}</div>
+                    ) : null}
+                  </Label>
+                  <Label className="mt-2">
+                    <span className="block">Senha</span>
+                    <Field
+                      className="mt-1 border border-opacity-50 w-full text-sm rounded-md p-2"
+                      type="password"
+                      id="password"
+                      placeholder="Digite sua senha"
+                      name="password"
+                    />
+                    {errors.password ? (
+                      <div className="text-xs text-red-300">
+                        {errors.password}
+                      </div>
+                    ) : null}
+                  </Label>
+                  <Label check={true} className="mt-2">
+                    <Field className="" type="checkbox" name="acceptTerms" />
+                    <span className="ml-2">
+                      Criar uma conta significa que você concorda com nossos{" "}
+                      <Link href="/accept-terms" passHref>
+                        <a className="text-gray-800 font-medium hover:underline hover:text-gray-600">
+                          Termos de Serviço,
+                        </a>
+                      </Link>
+                      <Link href="/policies" passHref>
+                        <a className="text-gray-800 font-medium hover:underline hover:text-gray-600">
+                          Política de Privacidade
+                        </a>
+                      </Link>{" "}
+                      e nossas{" "}
+                      <Link href="/policies" passHref>
+                        <a className="text-gray-800 font-medium hover:underline hover:text-gray-600">
+                          Configurações de Notificações.
+                        </a>
+                      </Link>
+                      {errors.acceptTerms ? (
+                        <div className="text-xs text-red-300">
+                          {errors.acceptTerms}
+                        </div>
+                      ) : null}
+                    </span>
+                  </Label>
+                  <Button
+                    className="mt-4 w-full bg-purple-600 rounded-md text-white text-lg font-bold py-3 hover:bg-purple-600"
+                    block={false}
+                    size="large"
+                    type="submit"
+                  >
+                    Cadastrar
+                  </Button>
+                </Form>
+              )}
             </Formik>
 
             <hr className="my-3" />
@@ -137,9 +181,9 @@ export default function Signup() {
                 <Image src={DevplenoLogo} alt="Logo" height={25} width={81} />
               </div>
             </div>
-            {siteKey && (
+            {/* {siteKey && (
               <ReCaptcha ref={captchaRef} size="invisible" sitekey={siteKey} />
-            )}
+            )} */}
           </div>
         </main>
       </div>
